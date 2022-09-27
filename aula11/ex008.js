385187978760665a01f2f8b4020fe1398ad63a46
Felipe Moura Mainardes <flipe.mainardes@hotmail.com>
2022-09-26 22:36:31 -0300

    novos exercicios

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 59
+console.log(`A velocidade do seu carro é ${vel}`)
+if(vel > 60) { // condição simples, não há o "Se não"
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log (`Dirija sempre com cuidado`)
