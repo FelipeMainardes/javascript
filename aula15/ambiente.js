de9fcef2b657e61985052e5b735028a37b06ba74
Felipe Moura Mainardes <flipe.mainardes@hotmail.com>
2022-09-29 23:00:03 -0300

    aula 15

---
+++ aula15/ambiente.js
@@ -0,0 +1,8 @@
+let num = [5,8,4]
+num[3] = 6
+num.push(7)
+num.length // sem ()0
+
+console.log(`O vetor tem ${num.length} posições`)
+let pos = num.indexOf(8)
+console.log( `O valor 8 está na posição ${pos}`)
